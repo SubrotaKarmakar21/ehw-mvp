232d412315823921a9a06074284d629508df9107
Ubuntu <ubuntu@ip-172-31-40-19.ap-south-1.compute.internal>
2026-05-02 02:31:18 +0530

    Rebuilt repo with clean structure

--- includes-nct-gitsafe/linkedin/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('../config-nct.php');
-
-$client_id = LINKEDIN_APP_ID; 
-$client_secret = LINKEDIN_APP_SEC; 
-$redirect_uri =SITE_INC."linkedin/callback.php"; // enter your redirect url
-	
-?>
